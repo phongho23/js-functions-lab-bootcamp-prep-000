401196e36a33db852082ae814d474d15d8ec72cd
Phong Ho <ho.phongt@gmail.com>
2018-02-26 05:19:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`;
+function holidayCountdown(days, holiday) {
+  return `It's ${days}, ${name}!`;
 }
 
